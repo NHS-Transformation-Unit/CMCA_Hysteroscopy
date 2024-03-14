5d2d4af4e04411ce69566c27039aaa4de2185a6d
ASW-Analyst <andy.wilson8@nhs.net>
2024-03-14 14:07:07 +0000

    Create outputs script to run individual output files.

---
+++ src/outputs/outputs.R
@@ -0,0 +1,13 @@
+
+# Load Themes -------------------------------------------------------------
+
+source(paste0(here(), "/src/config/outputs/palette.R"))
+source(paste0(here(), "/src/config/outputs/tu_ggplot_themes.R"))
+
+# Run Model Output Scripts ------------------------------------------------
+
+source(paste0(here(), "/src/outputs/referrals_outputs.R"))
+source(paste0(here(), "/src/outputs/scan_outputs.R"))
+source(paste0(here(), "/src/outputs/telephone_clinic_outputs.R"))
+source(paste0(here(), "/src/outputs/rac_outputs.R"))
+source(paste0(here(), "/src/outputs/hysteroscopy_settings_outputs.R"))
